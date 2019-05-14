9574167fe55a1699450fdd88bcdec87f11587e3c
Cody <chodywhooch@live.ca>
2019-05-13 23:05:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,28 +12,30 @@ def resource_path(relative_path):
 
 
 while True:
+    # Py charm is annoying without having these in here somewhere as constants.
+    # I know I could get rid of the notification, but sometimes it can be helpful
+
     lang = 'ERROR'
     font = ImageFont.truetype("arial.ttf", 15)
     fcolor = 'white'
+    
+    # Here's the 'main menu'
+    
     for main in range(100):
         print('Which language would you like to translate to?')
         try:
             q1 = int(input('1. Davek\n2. Iokharic\n3. Rellanic\n9. Exit\n'))
             if q1 == 1:
-                text_length = 650
                 lang = 'Davek'
                 font = ImageFont.truetype(resource_path('Davek.otf'), 28, encoding='unic')
                 fcolor = 'silver'
                 break
             elif q1 == 2:
-                # iokharic()
-                text_length = 650
                 lang = 'Iokharic'
                 font = ImageFont.truetype(resource_path('Iokharic.otf'), 28, encoding='unic')
                 fcolor = 'red'
                 break
             elif q1 == 3:
-                text_length = 650
                 lang = 'Rellanic'
                 font = ImageFont.truetype(resource_path('Rellanic.otf'), 28, encoding='unic')
                 fcolor = 'darkgreen'
@@ -44,6 +46,9 @@ while True:
                 print('Please enter a valid response')
         except ValueError:
             print('Please enter a valid response(Use numbers to select)')
+
+    # Here's where the 'magic' happens haha
+
     print(f'{lang} chosen')
     string = input('Enter English\n')
     titlestring = string.replace('?', '_')
@@ -51,10 +56,12 @@ while True:
     fill_string = fill(newstring, 50)
 
     text_width, text_height = font.getsize(string)
+
+    # This was a quick way at attempting to make a dynamic box based on the text being filled
+    
     count = 0
     for count in range(len(string[::50])):
         count += 2
-    # print(count)
     if len(string) > 150:
         real_text_height = len(string) * count // 6
     elif len(string) > 125:
@@ -69,11 +76,13 @@ while True:
         real_text_height = len(string) * count // 1
     else:
         real_text_height = text_height + 10
-    canvas = Image.new('RGB', (text_length, real_text_height), 'black')
+    
+    # Making the Image
+    
+    canvas = Image.new('RGB', (650, real_text_height), 'black')
     draw = ImageDraw.Draw(canvas)
     draw.text((5, 5), fill_string, fcolor, font)
 
     canvas.save(f'{i}. {lang}' + '.png', "PNG")
     canvas.save(f'{i}. {lang} -' + titlestring[:15] + '.png', "PNG")
     i += 1
-    # canvas.show()
